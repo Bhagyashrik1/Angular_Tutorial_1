2675b694cb6bbef66bd75ccc72812fbad5197c28
bhagyashri <bhagyashreedesai6369@gmail.com>
2024-08-02 18:49:41 +0530

    see user task

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-type User = {
-    id: string;
-    avatar: string;
-    name : string;
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
 }
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -11,12 +19,14 @@ type User = {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) user!: User;
- 
+  @Input({ required: true }) user!: User;
+  @Input({ required: true}) selected! : boolean;
   @Output() select = new EventEmitter();
+
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
   }
+
   onSelectUser() {
     this.select.emit(this.user.id);
   }
