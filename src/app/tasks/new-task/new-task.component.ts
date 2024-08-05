653cd833acea21667953f62ee35ee30e07636a4b
bhagyashri <bhagyashreedesai6369@gmail.com>
2024-08-05 19:05:23 +0530

    add new task and display it

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,16 +1,29 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type newTaskData } from './new-task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>()
-
+  @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<newTaskData>();
+  enteredTitle = " ";
+  enteredSummery =" ";
+  enteredDueDate =" ";
   onCancel() {
     this.cancel.emit();
+  }
+  onSubmit(){
+    this.add.emit({
+      title : this.enteredTitle,
+      summery: this.enteredSummery,
+      date: this. enteredDueDate
+    });
+
   }
 }
---
+++ src/app/tasks/new-task/new-task.model.ts
@@ -0,0 +1,5 @@
+export interface newTaskData{
+    title: string ;
+    summery: string; 
+    date: string
+}
